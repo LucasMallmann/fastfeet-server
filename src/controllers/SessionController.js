40a732d0de42791a6f2bd18b83c7119a8f5e0c69
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-03-11 15:07:45 -0300

    Controller to return the token logged

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,4 +1,7 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
+import authConfig from '../config/auth';
 
 class SessionController {
   async store(req, res) {
@@ -12,7 +15,22 @@ class SessionController {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    return res.json({ message: 'oi' });
+    if (!(await user.checkPassword(password))) {
+      return res.status(404).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
